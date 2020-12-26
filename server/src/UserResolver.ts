42528c7858799cddbac9f09a152bbc0c74dff6e6
jithinlal <jithtitan007@gmail.com>
2020-12-26 12:20:46 +0530

    login

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,23 @@
-import { hash } from 'bcryptjs';
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import {
+	Arg,
+	Field,
+	Mutation,
+	ObjectType,
+	Query,
+	Resolver,
+} from 'type-graphql';
 import { User } from './entity/User';
+
+@ObjectType()
+class LoginResponse {
+	@Field()
+	accessToken: string;
+
+	@Field()
+	errorMessage: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -26,6 +43,37 @@ export class UserResolver {
 		} catch (error) {
 			console.error(error);
 			return false;
+		}
+	}
+
+	@Mutation(() => LoginResponse)
+	async login(
+		@Arg('email') email: string,
+		@Arg('password') password: string,
+	): Promise<LoginResponse> {
+		try {
+			const user = await User.findOne({ where: { email } });
+			if (!user) {
+				throw new Error('User does not exist!');
+			}
+
+			const valid = await compare(password, user.password);
+			if (!valid) {
+				throw new Error('Invalid login credentials!');
+			}
+
+			return {
+				accessToken: sign({ userId: user.id }, 'secret_random_string', {
+					expiresIn: '15m',
+				}),
+				errorMessage: '',
+			};
+		} catch (error) {
+			console.error(error);
+			return {
+				accessToken: '',
+				errorMessage: error.message,
+			};
 		}
 	}
 }
